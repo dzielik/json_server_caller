bd305ca80918540a4e7e8d1cffa44d7b581732c9
dzielik <139960179+dzielik@users.noreply.github.com>
2023-12-06 18:04:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -16,19 +16,19 @@ https.get('https://json-server-4isn.onrender.com/user', (resp) => {
   let data = '';
 
   // A chunk of data has been received.
-  resp.on('data', (chunk) => {
-    data += chunk;
-  });
+  //resp.on('data', (chunk) => {
+    //data += chunk;
+  //});
 
   // The whole response has been received. Print out the result.
   resp.on('end', () => {
-    console.log(JSON.parse(data));
+    console.log('alive');
   });
 
 }).on("error", (err) => {
   console.log("Error: " + err.message);
 });
-}, 10000);
+}, 300000);
 
 const html = `
 <!DOCTYPE html>

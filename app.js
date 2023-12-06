f2848baeefbc95b3786d52a0a43c8da214b77993
dzielik <139960179+dzielik@users.noreply.github.com>
2023-12-06 18:37:12 +0100

    Update app.js

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ https.get('https://json-server-4isn.onrender.com/user', (resp) => {
   let data = '';
 
   // A chunk of data has been received.
-  //resp.on('data', (chunk) => {
-    //data += chunk;
-  //});
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
 
   // The whole response has been received. Print out the result.
   resp.on('end', () => {
